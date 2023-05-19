50a06e5a6bc3b38a7f783496fa489d8fadaeb2af
Binamra Lamsal <binamra@skiff.com>
2023-05-20 00:57:15 +0545

    asdfasdf

--- nvchad-configs/mappings.lua
+++ nvchad-configs/mappings.lua
@@ -4,39 +4,172 @@ local M = {}
 local opts = { noremap = true, silent = true }
 
 M.general = {
-  n = {
-    -- Resize with arrows
-    ["<C-Up>"] = { ":resize -2<CR>", "Decrease height", opts },
-    ["<C-Down>"] = { ":resize +2<CR>", "Increase height", opts },
-    ["<C-Left>"] = { ":vertical resize -2<CR>", "Increase width", opts },
-    ["<C-Right>"] = { ":vertical resize +2<CR>", "Decrease width", opts },
+	n = {
+		-- Resize with arrows
+		["<C-Up>"] = { ":resize -2<CR>", "Decrease height", opts },
+		["<C-Down>"] = { ":resize +2<CR>", "Increase height", opts },
+		["<C-Left>"] = { ":vertical resize -2<CR>", "Increase width", opts },
+		["<C-Right>"] = { ":vertical resize +2<CR>", "Decrease width", opts },
 
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+		["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+		["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+		["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+		["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
 
-    -- Move text up and down
-    ["<A-j>"] = { "<Esc>:m .+1<CR>==g", "Move text down", opts },
-    ["<A-k"] = { "<Esc>:m .-2<CR>==g", "Move text up", opts },
+		-- Move text up and down
+		["<A-j>"] = { "<Esc>:m .+1<CR>==g", "Move text down", opts },
+		["<A-k"] = { "<Esc>:m .-2<CR>==g", "Move text up", opts },
 
-    -- Trouble.nvim
-    ["<leader>ee"] = { "<cmd>TroubleToggle<cr>", "Trouble Toggle" },
-    ["<leader>ew"] = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Trouble Toggle Workspace Diagnostics" },
-    ["<leader>ed"] = { "<cmd>TroubleToggle document_diagnostics<cr>", "Trouble Toggle Document Diagnostics" },
-    ["<leader>eq"] = { "<cmd>TroubleToggle quickfix<cr>", "Trouble Toggle Quickfixes" },
-    ["<leader>el"] = { "<cmd>TroubleToggle loclist<cr>", "Trouble Toggle loclist" },
-    ["<leader>gR"] = { "<cmd>TroubleToggle lsp_references<cr>", "Trouble Toggle LSP References" },
+		-- ["<C-m>"] = { "<cmd>NnnPicker %:p:h<CR>", "NnnPicker" },
 
-    ["<C-m>"] = { "<cmd>NnnPicker %:p:h<CR>", "NnnPicker" },
+		["-"] = { "<cmd>Oil<cr>", "Open Oil" },
+	},
+}
 
-    -- Nvim Dap
-    ["<leader>nb"] = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
-    ["<leader>nc"] = { "<cmd>lua require'dap'.continue()<cr>", "Continue Debugging" },
-    ["<leader>ni"] = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
-    ["<leader>no"] = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
-    ["<leader>nr"] = { "<cmd>lua require'dap'.repl.open()<cr>", "Open Repl" },
-  },
+M.trouble_nvim = {
+	n = {
+		["<leader>ee"] = { "<cmd>TroubleToggle<cr>", "Trouble Toggle" },
+		["<leader>ew"] = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Trouble Toggle Workspace Diagnostics" },
+		["<leader>ed"] = { "<cmd>TroubleToggle document_diagnostics<cr>", "Trouble Toggle Document Diagnostics" },
+		["<leader>eq"] = { "<cmd>TroubleToggle quickfix<cr>", "Trouble Toggle Quickfixes" },
+		["<leader>el"] = { "<cmd>TroubleToggle loclist<cr>", "Trouble Toggle loclist" },
+		["<leader>gR"] = { "<cmd>TroubleToggle lsp_references<cr>", "Trouble Toggle LSP References" },
+	},
+}
+
+M.lspconfig = {
+	plugin = true,
+
+	-- See `<cmd> :help vim.lsp.*` for documentation on any of the below functions
+	i = {
+		["<C-f>"] = {
+			function()
+				vim.lsp.buf.signature_help()
+			end,
+			"LSP signature help",
+		},
+	},
+
+	n = {
+		["gD"] = {
+			function()
+				vim.lsp.buf.declaration()
+			end,
+			"LSP declaration",
+		},
+
+		["gd"] = {
+			function()
+				vim.lsp.buf.definition()
+			end,
+			"LSP definition",
+		},
+
+		["K"] = {
+			function()
+				vim.lsp.buf.hover()
+			end,
+			"LSP hover",
+		},
+
+		["gi"] = {
+			function()
+				vim.lsp.buf.implementation()
+			end,
+			"LSP implementation",
+		},
+
+		["<leader>ls"] = {
+			function()
+				vim.lsp.buf.signature_help()
+			end,
+			"LSP signature help",
+		},
+
+		["<leader>D"] = {
+			function()
+				vim.lsp.buf.type_definition()
+			end,
+			"LSP definition type",
+		},
+
+		["<leader>ra"] = {
+			function()
+				require("nvchad_ui.renamer").open()
+			end,
+			"LSP rename",
+		},
+
+		["<leader>ca"] = {
+			function()
+				vim.lsp.buf.code_action()
+			end,
+			"LSP code action",
+		},
+
+		["gr"] = {
+			function()
+				vim.lsp.buf.references()
+			end,
+			"LSP references",
+		},
+
+		["<leader>f"] = {
+			function()
+				vim.diagnostic.open_float({ border = "rounded" })
+			end,
+			"Floating diagnostic",
+		},
+
+		["[d"] = {
+			function()
+				vim.diagnostic.goto_prev({ float = { border = "rounded" } })
+			end,
+			"Goto prev",
+		},
+
+		["]d"] = {
+			function()
+				vim.diagnostic.goto_next({ float = { border = "rounded" } })
+			end,
+			"Goto next",
+		},
+
+		["<leader>q"] = {
+			function()
+				vim.diagnostic.setloclist()
+			end,
+			"Diagnostic setloclist",
+		},
+
+		["<leader>fm"] = {
+			function()
+				vim.lsp.buf.format({ async = true })
+			end,
+			"LSP formatting",
+		},
+
+		["<leader>wa"] = {
+			function()
+				vim.lsp.buf.add_workspace_folder()
+			end,
+			"Add workspace folder",
+		},
+
+		["<leader>wr"] = {
+			function()
+				vim.lsp.buf.remove_workspace_folder()
+			end,
+			"Remove workspace folder",
+		},
+
+		["<leader>wl"] = {
+			function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end,
+			"List workspace folders",
+		},
+	},
 }
 
 return M
--- nvchad-configs/plugins.lua
+++ nvchad-configs/plugins.lua
@@ -3,242 +3,235 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+	-- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require("custom.configs.null-ls")
-        end,
-      },
-    },
-    config = function()
-      require("plugins.configs.lspconfig")
-      require("custom.configs.lspconfig")
-    end, -- Override to setup mason-lspconfig
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-    cmd = "NvimTreeToggle",
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+		cmd = "NvimTreeToggle",
+	},
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  {
-    "aca/emmet-ls",
-  },
+	{
+		"aca/emmet-ls",
+	},
 
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
 
-  { "b0o/schemastore.nvim",     ft = { "json" } },
+	{ "b0o/schemastore.nvim", ft = { "json" } },
 
-  {
-    "github/copilot.vim",
-    event = "BufEnter",
-  },
+	{
+		"github/copilot.vim",
+		event = "BufEnter",
+	},
 
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    event = "BufEnter",
-    opts = {
-      -- configurations go here
-    },
-  },
-  --
-  -- {
-  --   "RRethy/vim-illuminate",
-  --   lazy = false,
-  --   config = function()
-  --     require "custom.configs.vim-illuminate"
-  --   end,
-  -- },
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		event = "BufEnter",
+		opts = {
+			-- configurations go here
+		},
+	},
 
-  {
-    "folke/trouble.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("trouble").setup({})
-    end,
-  },
+	{
+		"folke/trouble.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({})
+		end,
+	},
 
-  -- {
-  --   "folke/noice.nvim",
-  --   -- lazy = false,
-  --   config = function()
-  --     require("noice").setup {
-  --       lsp = {
-  --         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-  --         override = {
-  --           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-  --           ["vim.lsp.util.stylize_markdown"] = true,
-  --           ["cmp.entry.get_documentation"] = true,
-  --         },
-  --         hover = {
-  --           enabled = false,
-  --         },
-  --         signature = {
-  --           enabled = false,
-  --         },
-  --       },
-  --       -- you can enable a preset for easier configuration
-  --       presets = {
-  --         bottom_search = true,         -- use a classic bottom cmdline for search
-  --         command_palette = true,       -- position the cmdline and popupmenu together
-  --         long_message_to_split = true, -- long messages will be sent to a split
-  --         inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-  --         lsp_doc_border = false,       -- add a border to hover docs and signature help
-  --       },
-  --     }
-  --   end,
-  --   dependencies = {
-  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-  --     "MunifTanjim/nui.nvim",
-  --     -- OPTIONAL:
-  --     --   `nvim-notify` is only needed, if you want to use the notification view.
-  --     --   If not available, we use `mini` as the fallback
-  --     "rcarriga/nvim-notify",
-  --   },
-  -- },
+	{
+		"folke/noice.nvim",
+		-- lazy = false,
+		config = function()
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+					hover = {
+						enabled = false,
+					},
+					signature = {
+						enabled = false,
+					},
+				},
+				-- you can enable a preset for easier configuration
+				presets = {
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
 
-  -- { "MunifTanjim/nui.nvim", lazy = false },
-  --
-  -- { "rcarriga/nvim-notify", lazy = false },
+	{ "MunifTanjim/nui.nvim", lazy = false },
 
-  {
-    "windwp/nvim-ts-autotag",
-    -- lazy = false,
-    ft = { "html", "jsx", "tsx" },
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end,
-  },
+	{ "rcarriga/nvim-notify", lazy = false },
 
-  -- {
-  --   "yamatsum/nvim-cursorline",
-  --   lazy = false,
-  --   config = function()
-  --     require("nvim-cursorline").setup {
-  --       cursorline = {
-  --         enable = true,
-  --         timeout = 1000,
-  --         number = false,
-  --       },
-  --       cursorword = {
-  --         enable = true,
-  --         min_length = 3,
-  --         hl = { underline = true },
-  --       },
-  --     }
-  --   end,
-  -- },
+	{
+		"windwp/nvim-ts-autotag",
+		ft = { "html", "jsx", "tsx" },
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
 
-  {
-    "rmagatti/auto-session",
-    config = function()
-      require("auto-session").setup({
-        log_level = "error",
-        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      })
-    end,
-  },
+	{
+		"rmagatti/auto-session",
+		config = function()
+			require("auto-session").setup({
+				log_level = "error",
+				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			})
+		end,
+	},
 
-  {
-    "barrett-ruth/live-server.nvim",
-    build = "npm i -g live-server",
-    ft = "html",
-    config = function()
-      require("live-server").setup()
-    end,
-  },
+	{
+		"barrett-ruth/live-server.nvim",
+		build = "npm i -g live-server",
+		ft = "html",
+		config = function()
+			require("live-server").setup()
+		end,
+	},
 
-  { "ThePrimeagen/vim-be-good", cmd = "VimBeGood" },
+	{ "ThePrimeagen/vim-be-good", cmd = "VimBeGood" },
 
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
-  },
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
+	},
 
-  {
-    "NvChad/nvim-colorizer.lua",
-    opts = {
-      user_default_options = {
-        tailwind = true,
-      },
-    },
-  },
+	{
+		"NvChad/nvim-colorizer.lua",
+		opts = {
+			user_default_options = {
+				tailwind = true,
+			},
+		},
+	},
+	--
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+			{
+				"tzachar/cmp-tabnine",
+				build = "./install.sh",
+				config = function()
+					local tabnine = require("cmp_tabnine.config")
+					tabnine:setup({}) -- put your options here
+				end,
+			},
+		},
+		opts = function(_, opts)
+			opts.formatting = {
+				format = require("tailwindcss-colorizer-cmp").formatter,
+			}
+			opts.sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "nvim_lua" },
+				{ name = "path" },
+				{ name = "cmp_tabnine" },
+			}
+		end,
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
-    },
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		cmd = "Oil",
+	},
 
-    opts = function(_, opts)
-      opts.formatting = {
-        format = require("tailwindcss-colorizer-cmp").formatter,
-      }
-    end,
-  },
+	{
+		"mvllow/modes.nvim",
+		tag = "v0.2.0",
+		event = "BufEnter",
+		config = function()
+			require("modes").setup()
+		end,
+	},
 
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "theHamsta/nvim-dap-virtual-text",
-      "rcarriga/nvim-dap-ui",
-      "mfussenegger/nvim-dap-python",
-      "nvim-telescope/telescope-dap.nvim",
-      { "leoluz/nvim-dap-go" },
-      { "jbyuki/one-small-step-for-vimkind" },
-      { "mxsdev/nvim-dap-vscode-js" },
-      {
-        "microsoft/vscode-js-debug",
-        opt = true,
-        run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-      },
-    },
+	{
+		"kosayoda/nvim-lightbulb",
+		dependencies = "antoinemadec/FixCursorHold.nvim",
+		event = "BufEnter",
+		config = function()
+			require("nvim-lightbulb").setup({ autocmd = { enabled = true } })
+		end,
+	},
 
-    config = function()
-      require("custom.dap.javascript").setup()
-    end,
-  },
-
-  {
-    "rcarriga/nvim-dap-ui",
-    config = true,
-  },
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		event = "BufEnter",
+		config = function()
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
 
 return plugins
